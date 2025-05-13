86c3898070deba9a7e596422d64c0241ce134c75
naya-h2 <hw7666@naver.com>
2025-05-14 00:15:26 +0900

    design: 도감탭 페이지 디자인 완성

--- src/app/(header)/collection/page.tsx
+++ src/app/(header)/collection/page.tsx
@@ -31,11 +31,53 @@ const DUMMY: {
     level: 1,
     type: 'togoCup',
   },
+  {
+    id: 36,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
+  {
+    id: 35,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
+  {
+    id: 34,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
+  {
+    id: 37,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
+  {
+    id: 33,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
+  {
+    id: 32,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
+  {
+    id: 31,
+    name: 'Binny',
+    level: 1,
+    type: 'togoCup',
+  },
 ];
 
 function CollectionPage() {
   return (
-    <div>
+    <div className="grid grid-cols-2 gap-y-5 justify-items-center">
       {DUMMY.map(({ id, name, level, type }) => (
         <CollectionCard key={id} name={name} level={level} type={type} />
       ))}
