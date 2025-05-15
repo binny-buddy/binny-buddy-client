3486b92b26bab704edbabac6ecc5273532cee38d
naya-h2 <hw7666@naver.com>
2025-05-15 15:31:58 +0900

    feat: detail 페이지 api 연결

--- src/app/binny/[id]/page.tsx
+++ src/app/binny/[id]/page.tsx
@@ -1,36 +1,38 @@
 import { TYPE_ICON } from '@/components/CollectionCard';
-import { CharacterType } from '@/types/character';
+import { Binny } from '@/types/character';
 import Image from 'next/image';
 import EditIcon from '@/assets/icons/Icon/pen.svg';
 import SecondHeader from '@/components/Header/SecondHeader';
 import BinnyModel from '@/components/Modeling/BinnyModel';
 
-export const BINNY: {
-  id: number;
-  name: string;
-  level: number;
-  level_cur: number;
-  level_max: number;
-  recycle_num: number;
-  type: CharacterType;
-} = {
-  id: 2,
-  name: 'Binny',
-  level: 1,
-  level_cur: 50,
-  level_max: 500,
-  recycle_num: 5,
-  type: 'togoCup',
+const COLOR = {
+  border: {
+    cup: 'border-togo-300',
+    container: 'border-delivery-300',
+    bottle: 'border-bottle-300',
+  },
+  text: {
+    cup: 'text-togo-300',
+    container: 'text-delivery-300',
+    bottle: 'text-bottle-300',
+  },
 };
 
-function BinnyPage() {
+async function BinnyPage({ params }: any) {
+  const { id } = await params;
+  const data = (await (
+    await fetch(
+      `https://binny-buddy-server.kodori.dev/api-public/v1/binny/${id}`
+    )
+  ).json()) as Binny;
+
   return (
     <>
       <SecondHeader>Details</SecondHeader>
 
       <div className="min-h-dvh pt-24 px-4 h-full flex flex-col justify-between">
         <section className=" mx-auto w-[360px] h-[360px]">
-          <BinnyModel />
+          <BinnyModel type={data.binny_type} />
         </section>
 
         {/* Bottom Sheet */}
@@ -38,10 +40,10 @@ function BinnyPage() {
           {/* Binny Info */}
           <div className="flex flex-col gap-2">
             <div className="flex gap-4 items-center justify-center">
-              <Image src={TYPE_ICON[BINNY.type]} alt="recycle type icon" />
+              <Image src={TYPE_ICON[data.binny_type]} alt="recycle type icon" />
               <label className="flex gap-2 relative h-6">
                 <input
-                  defaultValue={BINNY.name}
+                  defaultValue={data.name}
                   className="text-XXL font-bold w-auto border-b-2 border-transparent focus:border-b-black focus:outline-none"
                 />
                 <Image
@@ -57,30 +59,41 @@ function BinnyPage() {
               <p className="text-XL">
                 Lv.{' '}
                 <span className="font-bold text-main-800">
-                  {String(BINNY.level).padStart(2, '0')}
+                  {String(data.level).padStart(2, '0')}
                 </span>
               </p>
               <p className="text-gray-400">
-                {String(BINNY.level_cur)} /{' '}
-                {String(BINNY.level_max).padStart(3, '0')}
+                {String(data.xp)} / {String(2000)}
               </p>
             </div>
             <div className="relative bg-main-50 h-2">
-              <div className={`absolute h-2 bg-main-400 ${'w-1/2'}`} />
+              <div
+                className={`absolute h-2 bg-main-400 `}
+                style={{ width: `${(data.xp / 2000) * 100}%` }}
+              />
             </div>
           </div>
 
           {/* Binny message */}
-          <div className="border border-togo-300 rounded-2xl px-5 py-6 flex flex-col gap-3">
+          <div
+            className={`border rounded-2xl px-5 py-6 flex flex-col gap-3 ${
+              COLOR.border[data.binny_type]
+            }`}
+          >
             <p>
-              Recycle <span className="text-togo-300">To-go cup</span> to help
-              Binny grow!
+              Recycle{' '}
+              <span className={COLOR.text[data.binny_type]}>To-go cup</span> to
+              help Binny grow!
             </p>
-            <p className="text-L text-togo-300 flex gap-2 items-center">
-              <Image src={TYPE_ICON[BINNY.type]} alt="recycle type icon" />
+            <p
+              className={`text-L flex gap-2 items-center ${
+                COLOR.text[data.binny_type]
+              }`}
+            >
+              <Image src={TYPE_ICON[data.binny_type]} alt="recycle type icon" />
               Recycled{' '}
               <span className="font-bold">
-                {String(BINNY.recycle_num).padStart(2, '0')}
+                {String(data.recycle_num ?? 0).padStart(2, '0')}
               </span>{' '}
               times
             </p>
--- src/components/Modeling/BinnyModel.tsx
+++ src/components/Modeling/BinnyModel.tsx
@@ -3,6 +3,7 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { useEffect } from 'react';
+import { BinnyType } from '@/types/character';
 
 const CupModel = () => {
   const { scene } = useGLTF('/modeling/cup.glb'); // GLTF 파일 경로
@@ -27,7 +28,53 @@ const CupModel = () => {
   );
 };
 
-const BinnyModel = () => {
+const ContainerModel = () => {
+  const { scene } = useGLTF('/modeling/delivery.glb'); // GLTF 파일 경로
+
+  useEffect(() => {
+    scene.traverse((child: any) => {
+      if (child.isMesh) {
+        child.castShadow = true; // 그림자를 생성
+        child.receiveShadow = true; // 그림자를 받을 수 있도록 설정
+      }
+    });
+  }, [scene]);
+
+  return (
+    <primitive
+      object={scene}
+      scale={3}
+      position={[0, -1, 0]}
+      rotation={[0, Math.PI / 10, 0]}
+      castShadow
+    />
+  );
+};
+
+const BottleModel = () => {
+  const { scene } = useGLTF('/modeling/pet.glb'); // GLTF 파일 경로
+
+  useEffect(() => {
+    scene.traverse((child: any) => {
+      if (child.isMesh) {
+        child.castShadow = true; // 그림자를 생성
+        child.receiveShadow = true; // 그림자를 받을 수 있도록 설정
+      }
+    });
+  }, [scene]);
+
+  return (
+    <primitive
+      object={scene}
+      scale={2.5}
+      position={[0, -1, 0]}
+      rotation={[0, Math.PI / 10, 0]}
+      castShadow
+    />
+  );
+};
+
+const BinnyModel = ({ type }: { type: BinnyType }) => {
   return (
     <div className="w-full h-[360px]">
       <Canvas
@@ -62,13 +109,15 @@ const BinnyModel = () => {
           position={[0, -1, 0]} // 약간 아래로 이동
           receiveShadow // 그림자를 받을 수 있도록 설정
         >
-          <planeGeometry args={[5, 5]} /> {/* 바닥 크기 */}
+          <planeGeometry args={[10, 10]} /> {/* 바닥 크기 */}
           <shadowMaterial
             transparent={true} // 투명도 활성화
             opacity={0.5} // 그림자만 보이도록 설정 (0.5는 그림자 투명도)
           />
         </mesh>
-        <CupModel />
+        {type === 'cup' && <CupModel />}
+        {type === 'bottle' && <BottleModel />}
+        {type === 'container' && <ContainerModel />}
         <OrbitControls />
       </Canvas>
     </div>
